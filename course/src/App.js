415db4dc70ee880ae3ec2688cf115a4f1c7f5dc0
JangJiyeon <yeonjijang@naver.com>
2023-09-27 01:09:12 +0900

    1주차 과제 제출

--- course/src/App.js
+++ course/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 function App() {
   const handleClick=()=>{
-    alert("클릭에 성공했습니다!");
+    alert("장지연");
   }
   return (
    <>
     <h2>클릭 해주세요!</h2>
-    <button onClick={handleClick}>✨🐨 Click ME! 🐨✨</button>
+    <button onClick={handleClick}>✨🐨 이름 🐨✨</button>
    </>
   );
 }
