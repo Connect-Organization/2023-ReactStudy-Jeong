e9b0ecf3a818e6b3780b814b0009aa37ab55ee25
{JeongJimin} <{wlmin0339@naver.com}>
2023-10-25 20:07:12 +0900

    1주차 과제 완료

--- course/src/App.js
+++ course/src/App.js
@@ -1,11 +1,16 @@
-//2주차
+// 1주차
+import logo from './logo.svg';
 import './App.css';
-import NameCard from './pages/NameCard';
+
 function App() {
+  const handleClick=()=>{
+    alert("정지민");
+  }
   return (
-    <>
-      <NameCard />
-    </>
+   <>
+    <h2>클릭해볼까??</h2>
+    <button onClick={handleClick}>✨🐨 Click ME! 🐨✨</button>
+   </>
   );
 }
 
