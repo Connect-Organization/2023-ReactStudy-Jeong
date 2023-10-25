22247b1ce3a98f5582864b11af346eb7f13983c7
{JeongJimin} <{wlmin0339@naver.com}>
2023-10-25 21:09:28 +0900

    3주차 과제 완료

--- course/src/App.js
+++ course/src/App.js
@@ -1,12 +1,32 @@
-// 2주차
+// 1주차
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   const handleClick=()=>{
+//     alert("정지민");
+//   }
+//   return (
+//    <>
+//     <h2>클릭해볼까??</h2>
+//     <button onClick={handleClick}>✨🐨 Click ME! 🐨✨</button>
+//    </>
+//   );
+// }
+//
+// export default App;
+
+
+// 2, 3주차
 import './App.css';
+import Market from './pages/Market';
 import NameCard from './pages/NameCard';
 function App() {
   return (
     <>
-      <NameCard />
+      <Market />
     </>
   );
 }
 
-export default App;
+export default App
---
+++ course/src/components/Price.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { AppContext } from "../pages/Market";
+
+export default function Price(){
+    const {items} = useContext(AppContext);
+    return(
+        <div>
+            <h2>Price List 💰</h2>
+            {items.map(item => {
+                return(
+                    <div key={item.id}>
+                    	<div>
+                            {item.name}: ${item.price}
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
